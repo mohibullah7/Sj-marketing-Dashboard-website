122853b3186a45a737e6094bc498b78c31c7913a
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-09-08 01:39:20 +0500

    almost front complted

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+// const canvas = document.getElementById('offcanvas');
+
+// function show() {
+    
+//     if (canvas.classList.contains('d-none')) {
+//         canvas.classList.remove('d-none');
+//     } else {
+//         canvas.classList.add('d-none');
+//     }
+// }
+
+
 const canvas = document.getElementById('offcanvas');
 
 function show() {
-    // Toggle the d-none class based on visibility
     if (canvas.classList.contains('d-none')) {
         canvas.classList.remove('d-none');
+        canvas.style.width = '300px'; 
     } else {
         canvas.classList.add('d-none');
+        canvas.style.width = ''; 
     }
 }
+
+
+
